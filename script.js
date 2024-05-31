dff5dde585253864925a352cf5cf79c356eb3a19
corasinth <beforeyouknowkindness@gmail.com>
2024-05-31 17:13:14 -0500

    add super word checker

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const { log } = require('console')
+const { log, Console } = require('console')
 const fs = require('fs')
 
 // Generates a list of words that are acceptable for a given puzzle
-function wordSearch(acceptableLetters, centerLetter){
+function wordSearch(acceptableLetters, centerLetter) {
     // Reference alphabet string and case matching input letters
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
     acceptableLetters = acceptableLetters.toLowerCase()
     centerLetter = centerLetter.toLowerCase()
-    
+
+    // Arrays to distinguish the list of words and the list of words that use every letter
+    let wordsList = []
+    let superWordList = []
+
     // Putting words in memory
     const dict = JSON.parse(fs.readFileSync('./words.json', 'utf8', (err, data) => {
         if (err) {
@@ -17,34 +21,38 @@ function wordSearch(acceptableLetters, centerLetter){
         return data
     }))
 
-    
+
     // Create an alphabet excluding the acceptable letters for the pattern matching logic
-    let acceptableLettersReg = new RegExp (`[${acceptableLetters}]`, "i")
+    let acceptableLettersReg = new RegExp(`[${acceptableLetters}]`, "i")
     let unacceptableLetters = ""
 
-    for(let letter of alphabet){
-        if(letter.match(acceptableLettersReg) || letter === centerLetter){
+    for (let letter of alphabet) {
+        if (letter.match(acceptableLettersReg) || letter === centerLetter) {
             continue
         }
         unacceptableLetters += letter
-    }    
+    }
 
     // Creating regexes for matching the center letter and ensuring no unacceptable letters are present
     let centerLetterReg = new RegExp(`${centerLetter}`, "i")
-    let unacceptableLettersReg = new RegExp (`[${unacceptableLetters}]`,"i")
+    let unacceptableLettersReg = new RegExp(`[${unacceptableLetters}]`, "i")
+    // Regex uses positive lookahead to ensure the following conditions are met: that there is, for every letter passed into the function, at least one instance of any character followed by that letterg
+    let superWordReg = new RegExp(`(?=.*${acceptableLetters[0]})(?=.*${acceptableLetters[1]})(?=.*${acceptableLetters[2]})(?=.*${acceptableLetters[3]})(?=.*${acceptableLetters[4]})(?=.*${acceptableLetters[5]})(?=.*${centerLetter}).+`)
 
-    console.log(unacceptableLetters);
-    
+
     // Loop through 4-letter word dictionary and find all words that have the center letter at least once and which don't contain any unacceptable letters
+    // Also keep track of words that contain every letter
     for (let word of dict) {
         if (word.match(centerLetterReg) && !word.match(unacceptableLettersReg)) {
-            // list.push(word)
-           console.log(word)
+            // If a super word match is found we skip this iteration so it doesn't get added to both lists
+            if (word.match(superWordReg)) {
+                superWordList.push(word)
+                console.log(word);
+                continue
+            }
+            wordsList.push(word)
         }
-        
     }
-
-    // fs.writeFileSync("./words.json", JSON.stringify(list))
-
+    
 }
 wordSearch('craonv', "d")
