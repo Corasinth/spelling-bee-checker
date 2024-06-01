33ac299f8d65aa1e4a44699d784b8c5a04c3e96d
corasinth <beforeyouknowkindness@gmail.com>
2024-05-31 20:19:28 -0500

    add reveal words button logic

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,19 +8,28 @@ let puzzleLettersForm = document.getElementById('puzzleLettersForm')
 let searchCompleteMessageEl = document.getElementById('searchCompleteMessage')
 let wordsFoundEl = document.getElementById('wordsFound')
 
+let outerLetters = document.getElementById("outerLetters").value
+let centerLetter = document.getElementById("centerLetter").value
+
+
+let revealAllEl = document.getElementById('revealAll')
+let revealWordsEl = document.getElementById('revealWords')
+let revealPanagramsEl = document.getElementById('revealPanagrams')
+
+let wordsListEl = document.getElementById('wordsList')
 
 // ============================= EVENT LISTENERS =============================
 puzzleLettersForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    
-    const outerLetters = document.getElementById("outerLetters").value
-    const centerLetter = document.getElementById("centerLetter").value
 
-    let solutionObject = wordSearch(outerLetters, centerLetter)
+    outerLetters = document.getElementById("outerLetters").value
+    centerLetter = document.getElementById("centerLetter").value
 
+
+    solutionObject = wordSearch(outerLetters, centerLetter)
     let panagramPlural = 'panagrams'
 
-    if (solutionObject.pangramWords.length === 1){
+    if (solutionObject.pangramWords.length === 1) {
         panagramPlural = 'panagram'
     }
 
@@ -28,25 +37,31 @@ puzzleLettersForm.addEventListener("submit", (e) => {
     wordsFoundEl.textContent = `${solutionObject.words.length} words and ${solutionObject.pangramWords.length} ${panagramPlural} found.`
 
     searchCompleteMessageEl.style = 'display: initial;'
-  });
+});
 
-  puzzleLettersForm.addEventListener("submit", (e) => {
+revealAllEl.addEventListener("click", (e) => {
     e.preventDefault();
-  });
+    displayWords(solutionObject.words.concat(solutionObject.pangramWords).sort())
+});
 
-  puzzleLettersForm.addEventListener("submit", (e) => {
+revealWordsEl.addEventListener("click", (e) => {
     e.preventDefault();
-    
-  });
+    displayWords(solutionObject.words)
+});
+
+revealPanagramsEl.addEventListener("click", (e) => {
+    e.preventDefault();
+    displayWords(solutionObject.pangramWords)
+});
 
 
 // ============================= FUNCTIONS =============================
 
 // Generates a list of words that are acceptable for a given puzzle
-function wordSearch(OuterLetters, centerLetter) {
+function wordSearch(outerLetters, centerLetter) {
     // Reference alphabet string and case matching input letters
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    OuterLetters = OuterLetters.toLowerCase()
+    outerLetters = outerLetters.toLowerCase()
     centerLetter = centerLetter.toLowerCase()
 
     // Arrays to distinguish the list of words and the list of words that use every letter
@@ -55,28 +70,29 @@ function wordSearch(OuterLetters, centerLetter) {
 
 
     // Create an alphabet excluding the acceptable letters for the pattern matching logic
-    let OuterLettersReg = new RegExp(`[${OuterLetters}]`, "i")
-    let unOuterLetters = ""
+    let outerLettersReg = new RegExp(`[${outerLetters}]`, "i")
+    let unacceptableLetters = ""
 
     for (let letter of alphabet) {
-        if (letter.match(OuterLettersReg) || letter === centerLetter) {
+        if (letter.match(outerLettersReg) || letter === centerLetter) {
             continue
         }
-        unOuterLetters += letter
+        unacceptableLetters += letter
     }
 
     // Creating regexes for matching the center letter and ensuring no unacceptable letters are present
     let centerLetterReg = new RegExp(`${centerLetter}`, "i")
-    let unOuterLettersReg = new RegExp(`[${unOuterLetters}]`, "i")
+    let unacceptableLettersReg = new RegExp(`[${unacceptableLetters}]`, "i")
     // Regex uses positive lookahead to ensure the following conditions are met: that there is, for every letter passed into the function, at least one instance of any character followed by that letterg
-    let pangramWordReg = new RegExp(`(?=.*${OuterLetters[0]})(?=.*${OuterLetters[1]})(?=.*${OuterLetters[2]})(?=.*${OuterLetters[3]})(?=.*${OuterLetters[4]})(?=.*${OuterLetters[5]})(?=.*${centerLetter}).+`)
+    let pangramWordReg = new RegExp(`(?=.*${outerLetters[0]})(?=.*${outerLetters[1]})(?=.*${outerLetters[2]})(?=.*${outerLetters[3]})(?=.*${outerLetters[4]})(?=.*${outerLetters[5]})(?=.*${centerLetter}).+`)
+
 
 
     // Loop through 4-letter word dictionary and find all words that have the center letter at least once and which don't contain any unacceptable letters
     // Also keep track of words that contain every letter
     // Dictionary variable comes from words.js
     for (let word of dictionary) {
-        if (word.match(centerLetterReg) && !word.match(unOuterLettersReg)) {
+        if (word.match(centerLetterReg) && !word.match(unacceptableLettersReg)) {
             // If a pangram word match is found we skip this iteration so it doesn't get added to both lists
             if (word.match(pangramWordReg)) {
                 pangramWordList.push(word)
@@ -90,4 +106,21 @@ function wordSearch(OuterLetters, centerLetter) {
         pangramWords: pangramWordList
     }
 }
-// wordSearch('craonv', "d")
+
+function displayWords(wordArr) {
+    let pangramWordReg = new RegExp(`(?=.*${outerLetters[0]})(?=.*${outerLetters[1]})(?=.*${outerLetters[2]})(?=.*${outerLetters[3]})(?=.*${outerLetters[4]})(?=.*${outerLetters[5]})(?=.*${centerLetter}).+`)
+
+    let wordsListHTML = ""
+
+    for (let word of wordArr) {
+        if (word.match(pangramWordReg)) {
+            // Adds special class for panagrams
+            wordsListHTML += `<li class= "panagram">${word}</li>`
+            continue
+        }
+
+        wordsListHTML += `<li>${word}</li>`
+    }
+
+    wordsListEl.innerHTML = wordsListHTML
+}
